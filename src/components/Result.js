6398512a7aedf42e8f6488861b9d58dde3a1f896
JayjayMatt <jmatth16@student.aau.dk>
2020-06-09 16:45:18 +0200

    include tracker (work in progress)

--- src/components/Result.js
+++ src/components/Result.js
@@ -14,6 +14,7 @@ class Result extends Component {
   }
 
   render() {
+    this.setCookies();
     return this.renderResult();
   }
 
@@ -46,6 +47,36 @@ class Result extends Component {
     );
   }
 
+  
+  setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  setCookies(){
+    const highScore = this.getCookie('highScore');
+    this.setCookie('lastScore', this.props.score, 365);
+    if(highScore === '' || parseInt(highScore) < this.props.score) {
+      this.setCookie('highScore', this.props.score, 365);
+    }
+  }
   openModal() {
     this.setState({
       showModal: true
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
+// import "./scripts/mousetrack/js/src/load.js"
 
 ReactDOM.render(<App />, document.getElementById("root"));
